72730d49da2a162927ae07c5e3d84192693123fe
ETMANZI <eugenemanzi03@gmail.com>
2026-03-26 14:01:52 +0200

    Fix register page error han73

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,10 @@
+
+
+
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: window.location.origin + "/api",
 });
 
 api.interceptors.request.use((config) => {
@@ -11,3 +14,49 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
+
+
+
+
+
+
+
+
+// import axios from "axios";
+
+// const API_BASE_URL =
+//   import.meta.env.VITE_API_BASE_URL ||
+//   "https://market-hub-app-production-7142.up.railway.app/api";
+
+// export const api = axios.create({
+//   baseURL: API_BASE_URL,
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("access_token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+
+
+
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: "http://127.0.0.1:8000/api",
+
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("access_token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+
+
