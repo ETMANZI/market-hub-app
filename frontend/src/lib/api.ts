3c5412d76c25b0ad4a2fb7a745b5b05c736813a2
ETMANZI <eugenemanzi03@gmail.com>
2026-03-26 14:48:52 +0200

    Fix production API base URL

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,10 +1,7 @@
-
-
-
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: window.location.origin + "/api",
+  baseURL: `${window.location.origin}/api`,
 });
 
 api.interceptors.request.use((config) => {
@@ -14,46 +11,3 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
-
-
-
-
-// import axios from "axios";
-
-// const API_BASE_URL =
-//   import.meta.env.VITE_API_BASE_URL ||
-//   "https://market-hub-app-production-7142.up.railway.app/api";
-
-// export const api = axios.create({
-//   baseURL: API_BASE_URL,
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("access_token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-
-
-
-// import axios from "axios";
-
-// export const api = axios.create({
-//   baseURL: "http://127.0.0.1:8000/api",
-
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("access_token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-
-
-
